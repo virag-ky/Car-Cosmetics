18c3e2008f0ce20fca2a34e96ce548fd068add9b
Virag Kormoczy <vkormoczy@gmail.com>
2023-05-06 15:30:44 +0300

    Add menu icons

--- app/components/MobileMenu.jsx
+++ app/components/MobileMenu.jsx
@@ -1,36 +1,45 @@
 import Link from 'next/link';
+import { ImHome } from 'react-icons/im';
+import { ImProfile } from 'react-icons/im';
+import { MdAttachMoney } from 'react-icons/md';
+import { MdMiscellaneousServices } from 'react-icons/md';
+import { GrGallery } from 'react-icons/gr';
+import styles from '../styles/navbar/navbar.module.css';
 
 const MobileMenu = ({ handleMenu }) => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link onClick={handleMenu} href="/">
-            Fő oldal
-          </Link>
-        </li>
-        <li>
-          <Link onClick={handleMenu} href="/about">
-            Rólunk
-          </Link>
-        </li>
-        <li>
-          <Link onClick={handleMenu} href="/prices">
-            Árlista
-          </Link>
-        </li>
-        <li>
-          <Link onClick={handleMenu} href="/services">
-            Szolgáltatások
-          </Link>
-        </li>
-        <li>
-          <Link onClick={handleMenu} href="/gallery">
-            Galéria
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <ul className={styles.menu_list}>
+      <li>
+        <ImHome size="1.2rem" />
+        <Link onClick={handleMenu} href="/">
+          Fő oldal
+        </Link>
+      </li>
+      <li>
+        <ImProfile size="1.2rem" />
+        <Link onClick={handleMenu} href="/about">
+          Rólunk
+        </Link>
+      </li>
+      <li>
+        <MdAttachMoney size="1.3rem" />
+        <Link onClick={handleMenu} href="/prices">
+          Árlista
+        </Link>
+      </li>
+      <li>
+        <MdMiscellaneousServices size="1.3rem" />
+        <Link onClick={handleMenu} href="/services">
+          Szolgáltatások
+        </Link>
+      </li>
+      <li>
+        <GrGallery size="1.2rem" />
+        <Link onClick={handleMenu} href="/gallery">
+          Galéria
+        </Link>
+      </li>
+    </ul>
   );
 };
 
