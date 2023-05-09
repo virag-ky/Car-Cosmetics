a47e90072979c29302e6b58d6e85a85b9792fc4d
Virag Kormoczy <vkormoczy@gmail.com>
2023-05-09 20:26:14 +0300

    Add main image

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import Logo from './components/Logo';
 import { FaHandshake } from 'react-icons/fa';
 import { TbTargetArrow } from 'react-icons/tb';
@@ -9,7 +10,8 @@ const HomePage = () => {
   return (
     <main className={styles.main}>
       <header className={styles.header}>
-        <h1>Car Detailing KFT</h1>
+        <h1>Pro Polír</h1>
+        <h2>Car Detailing KFT</h2>
         <Logo styles={styles} />
         <Link role="button" className={styles.button} href="/about">
           Rólunk
@@ -18,17 +20,24 @@ const HomePage = () => {
       <section className={styles.intro_section}>
         <div className={styles.keywords_container}>
           <div className={styles.keyword}>
-            <FaHandshake size="1rem" />
+            <FaHandshake size="1.8rem" color="#555555" />
             <span>Bizalom</span>
           </div>
           <div className={styles.keyword}>
-            <BiBadgeCheck />
+            <BiBadgeCheck size="1.8rem" color="#555555" />
             <span>Minőség</span>
           </div>
           <div className={styles.keyword}>
-            <TbTargetArrow />
+            <TbTargetArrow size="1.8rem" color="#555555" />
             <span>Precizitás</span>
           </div>
+        </div>
+        <div className={styles.main_image_container}>
+          <Image
+            src="/images/main.png"
+            alt="owners of the business with a red car"
+            fill={true}
+          />
         </div>
       </section>
     </main>
